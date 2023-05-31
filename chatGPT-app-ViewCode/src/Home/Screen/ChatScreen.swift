426a25dfb27ed4bdacda8741ca703ce52e1e6fde
Alysson Menezes <almenezes912@gmail.com>
2023-05-30 21:45:07 -0300

    configurando protocolos da textFIeld (na controller), adicionando animacao e verificação no sendButton.

--- chatGPT-app-ViewCode/src/Home/Screen/ChatScreen.swift
+++ chatGPT-app-ViewCode/src/Home/Screen/ChatScreen.swift
@@ -6,39 +6,130 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ChatScreen: UIView {
 
-    lazy var genericLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Ola Mundo !"
-        label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 45, weight: .bold)
-        return label
+    var player: AVAudioPlayer?
+    
+    lazy var messageInputView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .backGround
+        return view
     }()
    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = .red
-        addViews()
-        setupConstraints()
+    lazy var messageBarView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .appLight
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        return view
+    }()
+   
+    lazy var sendButton: UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.backgroundColor = .orange
+        btn.clipsToBounds = true
+        btn.layer.cornerRadius =  15 //25.5
+        btn.layer.shadowRadius = 10
+        btn.layer.shadowOffset = CGSize(width: 0, height: 5)
+        btn.layer.shadowOpacity = 0.3
+        btn.setImage(UIImage(systemName: "paperplane.circle.fill"), for: .normal)
+        btn.tintColor = .white
+        btn.isEnabled = false
+        btn.transform = .init(scaleX: 0.8, y: 0.8)// nasce com tamanho menor depois aumenta
+        btn.addTarget(self, action: #selector(tappedSendButton), for: .touchUpInside)
+        return btn
+    }()
+    
+
+    
+    lazy var inputMessageTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.autocorrectionType = .no
+        tf.backgroundColor = .white
+        tf.borderStyle = .roundedRect
+        tf.keyboardType = .asciiCapable
+        tf.placeholder = "Digite aqui"
+        tf.font = UIFont.helveticaNeueMedium(size: 16) // customFont+extension
+       // tf.isSecureTextEntry = false
+        return tf
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none // retirando linhas
+        tableView.showsVerticalScrollIndicator = false // desativando scrool indicator
+        // TO DO: Register
+        tableView.backgroundColor = .clear
+        tableView.transform = CGAffineTransform(scaleX: 1, y: -1) // tableView Contraria/ append item de baixo pra cima
+        return tableView
+    }()
+    
+    @objc func tappedSendButton(){
+        print(#function)
     }
     
-    private func addViews(){
-        addSubview(genericLabel)
+    public func setupTextFieldDelegate(delegate: UITextFieldDelegate){
+        inputMessageTextField.delegate = delegate
+    }
+    public func setupTableViewProtocol(delegate: UITableViewDelegate, dataSource: UITableViewDataSource){
+        tableView.delegate = delegate
+        tableView.dataSource = dataSource
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .backGround
+        addElements()
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func addElements(){
+        addSubview(tableView)
+        addSubview(messageInputView)
+        addSubview(sendButton)
+        messageInputView.addSubview(messageBarView)
+        messageInputView.addSubview(inputMessageTextField)
+    }
     
     private func setupConstraints(){
         NSLayoutConstraint.activate( [
-            genericLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            genericLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+           
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: messageInputView.topAnchor),
             
+            messageInputView.bottomAnchor.constraint(equalTo: keyboardLayoutGuide.topAnchor),// layout sobe com teclado
+            messageInputView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            messageInputView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            messageInputView.heightAnchor.constraint(equalToConstant: 80),
+            
+            messageBarView.leadingAnchor.constraint(equalTo: messageInputView.leadingAnchor, constant: 20),
+            messageBarView.trailingAnchor.constraint(equalTo: messageInputView.trailingAnchor, constant: -20),
+            messageBarView.heightAnchor.constraint(equalToConstant: 55),
+            messageBarView.centerYAnchor.constraint(equalTo: messageInputView.centerYAnchor),
+            
+            sendButton.trailingAnchor.constraint(equalTo: messageBarView.trailingAnchor, constant: -15),
+            sendButton.heightAnchor.constraint(equalToConstant: 65),
+            sendButton.widthAnchor.constraint(equalToConstant: 65),
+            sendButton.bottomAnchor.constraint(equalTo: messageBarView.bottomAnchor, constant: -10),
+            
+            inputMessageTextField.leadingAnchor.constraint(equalTo: messageBarView.leadingAnchor, constant: 20),
+            inputMessageTextField.trailingAnchor.constraint(equalTo: sendButton.leadingAnchor, constant: -5),
+            
+            inputMessageTextField.heightAnchor.constraint(equalToConstant: 45),
+            inputMessageTextField.centerYAnchor.constraint(equalTo: messageBarView.centerYAnchor)
         ])
     }
 }
--- chatGPT-app-ViewCode/src/Home/ViewController/ChatVC.swift
+++ chatGPT-app-ViewCode/src/Home/ViewController/ChatVC.swift
@@ -16,22 +16,63 @@ class ChatVC: UIViewController {
     private var screen: ChatScreen?
     
     
+    
+    
     override func loadView() {
         screen = ChatScreen()
         view = screen
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        addLogoToNavigationBarItem(image: UIImage(named: "logo") ?? UIImage())
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        screen?.setupTableViewProtocol(delegate: self, dataSource: self)
+        screen?.setupTextFieldDelegate(delegate: self)
         viewModel.delegate(delegate: self)
         viewModel.featchMessage(message: "Cria uma funcao com swift")
-    
+        
     }
     
 
     
     
 }
+extension ChatVC: UITextFieldDelegate {
+   
+  
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text as NSString? else { return false }
+        let txtAfterUpdate = text.replacingCharacters(in: range, with: string)
+        
+        if txtAfterUpdate.isEmpty { // animation
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
+                self.screen?.sendButton.isEnabled = false
+                self.screen?.sendButton.transform = .init(scaleX: 0.8, y: 0.8)
+            }, completion: { _ in
+            })
+        } else { // animation
+            UIView.animate(withDuration: 0.3, delay: 0,usingSpringWithDamping: 0.4, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                self.screen?.sendButton.isEnabled = true
+                self.screen?.sendButton.transform = .identity
+            }, completion: { _ in
+            })
+            }
+        return true
+        }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    }
+    
+    
+
 
 extension ChatVC: ChatViewModelProtocol {
     func succes() {
@@ -40,6 +81,18 @@ extension ChatVC: ChatViewModelProtocol {
     
     func error(message: String) {
         print("Deu Ruim: \(message)")
+    }
+    
+}
+
+extension ChatVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
     }
     
     
